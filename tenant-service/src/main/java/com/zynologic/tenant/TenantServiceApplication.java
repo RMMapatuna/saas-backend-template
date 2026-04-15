26e6fe8cb94b7eeedf8cd9207640920bfc964bbe
RM <ravindum.mapatuna@gmail.com>
2026-04-15 12:37:39 +0800

    Add `tenant-service` module with basic configuration and dependencies

---
+++ tenant-service/src/main/java/com/zynologic/tenant/TenantServiceApplication.java
@@ -0,0 +1,11 @@
+package com.zynologic.tenant;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenantServiceApplication.class, args);
+    }
+}
